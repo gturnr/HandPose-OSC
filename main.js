46de28f0851419030c5c0c43fe2c690702aab764
Guy Turner <guyturner797@gmail.com>
2023-10-10 21:06:51 +0100

    Added support for Mac OS Webcams
    - Tested on Ventura.
    - Updated Electron and Electron builder to latest stable.
    - Added required entitlements, and extended info according to Electron documentation.

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const { app, BrowserWindow, Menu } = require("electron");
-const server = require('./index')
+
+const { systemPreferences } = require('electron');
+const camera = systemPreferences.askForMediaAccess('camera');
+
+const server = require('./index');
 
 
 function createWindow() {
 	// Create the browser window.
-	const win = new BrowserWindow({ width: 800, height: 600 });
+	const win = new BrowserWindow({ 
+		width: 800, 
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false
+		}
+	});
 	
 	//uncomment for debug console:
-	//win.webContents.openDevTools();
+	win.webContents.openDevTools();
 
 	// and load the html of the app.
 	win.loadFile("./camera.html");
